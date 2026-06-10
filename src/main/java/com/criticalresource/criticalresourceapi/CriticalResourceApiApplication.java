6ccf36216c720a497f8397c0d1a423f5b7d9f877
Stephane <stephane.loiseau.external@atos.net>
2026-06-10 11:01:09 +0200

    1.1 - Setup Maven project structure & dependencies

---
+++ src/main/java/com/criticalresource/criticalresourceapi/CriticalResourceApiApplication.java
@@ -0,0 +1,13 @@
+package com.criticalresource.criticalresourceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriticalResourceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriticalResourceApiApplication.class, args);
+	}
+
+}
